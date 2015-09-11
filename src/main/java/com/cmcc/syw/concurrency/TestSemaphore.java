23424773c376606aabc7c7ea6b1f3f6ac73fca9b
sunyiwei <514912821@qq.com>
2015-09-11 17:00:26 +0800

    练习semaphore的使用

---
+++ src/main/java/com/cmcc/syw/concurrency/TestSemaphore.java
@@ -0,0 +1,49 @@
+package com.cmcc.syw.concurrency;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sunyiwei on 2015/9/11.
+ */
+public class TestSemaphore {
+    public static void main(String[] args) {
+        final int count = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(count);
+        final Semaphore semaphore = new Semaphore(count);
+
+
+        int size = count * 2;
+        for (int i = 0; i < size; i++) {
+            service.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+
+                        //sleep
+                        Thread.currentThread().sleep(2000);
+                        System.out.println(Thread.currentThread().getName() + ": Hi...");
+
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        service.shutdown();
+        try {
+            while(!service.awaitTermination(10, TimeUnit.SECONDS)){
+                    continue;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
