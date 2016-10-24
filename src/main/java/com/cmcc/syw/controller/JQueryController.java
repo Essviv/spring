316641645b9aafc5fa6c0d3f18c00b45bf382eb6
essviv <514912821@qq.com>
2016-10-24 21:34:05 +0800

    add spring test practice code

--- src/main/java/com/cmcc/syw/controller/JQueryController.java
+++ src/main/java/com/cmcc/syw/controller/JQueryController.java
@@ -1,17 +1,19 @@
 package com.cmcc.syw.controller;
 
 import com.google.gson.Gson;
+
 import org.apache.commons.codec.Charsets;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by sunyiwei on 2016/2/5.
--- src/test/java/com/cmcc/syw/controller/JQueryControllerTest.java
+++ src/test/java/com/cmcc/syw/controller/JQueryControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+<<<<<<< 7f43d155a44ba27e5be73c2d43ab9efbc1e05f02
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -43,12 +44,36 @@ public class JQueryControllerTest {
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().string(content));
+                .param("test", "hello_world")
+                .header("Content-Type", "application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("hello_world")));
     }
 
     @Test
     public void testTest1() throws Exception {
         mockMvc.perform(post("/jquery/test"))
-            .andDo(print())
-            .andExpect(status().isOk());
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testTest4() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("get");
+        request.setRequestURI("/jquery/test4");
+        request.setParameter("name", "sunyiwei");
+        request.setParameter("value", "handsome-guy");
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        jQueryController.test4(request, response);
+
+        String output = "";
+
+        System.out.println(output);
+
+        assertNotNull(output);
     }
 }
