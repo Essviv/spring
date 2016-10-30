eddeabfb4f0ce83ddf5e6ac68abc97e9aff84e05
essviv <514912821@qq.com>
2016-10-30 08:59:33 +0800

    fix bug of merge

--- src/test/java/com/cmcc/syw/controller/JQueryControllerTest.java
+++ src/test/java/com/cmcc/syw/controller/JQueryControllerTest.java
@@ -4,14 +4,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-<<<<<<< 7f43d155a44ba27e5be73c2d43ab9efbc1e05f02
 import org.springframework.web.context.WebApplicationContext;
 
+import static junit.framework.TestCase.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,6 +31,9 @@ public class JQueryControllerTest {
     MockMvc mockMvc = null;
 
     @Autowired
+    JQueryController jQueryController;
+
+    @Autowired
     WebApplicationContext wac;
 
     @Before
@@ -44,11 +49,6 @@ public class JQueryControllerTest {
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().string(content));
-                .param("test", "hello_world")
-                .header("Content-Type", "application/json"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(is("hello_world")));
     }
 
     @Test
