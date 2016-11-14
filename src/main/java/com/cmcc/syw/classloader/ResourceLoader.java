e33f3a350fbd201bd7cef76520cbd4e97871a452
essviv <514912821@qq.com>
2016-11-14 09:58:09 +0800

    add getResourceAsStream code

---
+++ src/main/java/com/cmcc/syw/classloader/ResourceLoader.java
@@ -0,0 +1,29 @@
+package com.cmcc.syw.classloader;
+
+import org.apache.commons.codec.Charsets;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 资源加载器， 测试classLoader的getResourceAsStream方法
+ * <p/>
+ * Created by sunyiwei on 2016/11/14.
+ */
+public class ResourceLoader {
+    public static void load(String resourcePath) throws IOException {
+        InputStream is = ResourceLoader.class.getResourceAsStream(resourcePath);
+        if (is != null) {
+            System.out.println(StreamUtils.copyToString(is, Charsets.UTF_8));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String r3 = "/r3.txt";
+        final String r4 = "r3.txt";
+
+        ResourceLoader.load(r3);
+        ResourceLoader.load(r4);
+    }
+}
