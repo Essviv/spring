319d36ea80d92f328e21175c626684f867505372
essviv <514912821@qq.com>
2016-12-07 18:02:49 +0800

    add CLH lock impl

---
+++ src/main/java/com/cmcc/syw/concurrency/lock/ClhLock.java
@@ -0,0 +1,66 @@
+package com.cmcc.syw.concurrency.lock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * clh锁实践代码
+ * <p>
+ * Created by sunyiwei on 2016/12/7.
+ */
+public class ClhLock implements Operator {
+    //前驱节点
+    private ThreadLocal<Node> prev = new ThreadLocal<>();
+
+    //当前节点
+    private ThreadLocal<Node> current = new ThreadLocal<>();
+
+    //末尾节点
+    private AtomicReference<Node> tail = new AtomicReference<>();
+
+    public static void main(String[] args) {
+        ClhLock clhLock = new ClhLock();
+
+        final int COUNT = 10;
+        for (int i = 0; i < COUNT; i++) {
+            new CustomThread(clhLock).start();
+        }
+    }
+
+    public void lock() {
+        //创建当前的节点
+        Node currentNode = new Node();
+        current.set(currentNode);
+
+        //加入队列
+        Node prevNode = tail.getAndSet(currentNode);
+        prev.set(prevNode);
+
+        //等待获取锁
+        while (prevNode != null && prevNode.isLocked()) {
+        }
+
+        //help GC
+        prev.set(null);
+    }
+
+    public void unlock() {
+        current.get().setLocked(false);
+    }
+
+    private class Node {
+        //标识当前线程是否正在获取锁或已经获取到锁
+        private volatile boolean isLocked;
+
+        public Node() {
+            this.isLocked = true;
+        }
+
+        public boolean isLocked() {
+            return isLocked;
+        }
+
+        public void setLocked(boolean locked) {
+            isLocked = locked;
+        }
+    }
+}
--- src/main/java/com/cmcc/syw/concurrency/lock/CustomThread.java
+++ src/main/java/com/cmcc/syw/concurrency/lock/CustomThread.java
@@ -2,10 +2,11 @@ package com.cmcc.syw.concurrency.lock;
 
 /**
  * 测试各种锁的线程
- *
+ * <p>
  * Created by sunyiwei on 2016/12/6.
  */
 public class CustomThread extends Thread {
+    private static int value;
     private final Operator operator;
 
     public CustomThread(Operator operator) {
@@ -14,10 +15,11 @@ public class CustomThread extends Thread {
 
     @Override
     public void run() {
-        while (true) {
+        final int COUNT = 10;
+        for (int i = 0; i < COUNT; i++) {
             operator.lock();
 
-            System.out.println(Thread.currentThread());
+            System.out.println(Thread.currentThread() + ": " + String.valueOf(value++));
 
             operator.unlock();
         }
