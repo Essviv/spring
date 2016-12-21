0b17713c72429c14fd22f7b300d5180f74383f8e
essviv <514912821@qq.com>
2016-12-21 19:55:04 +0800

    add equals and hashCode practise code

---
+++ src/main/java/com/cmcc/syw/basic/RightEquals.java
@@ -0,0 +1,36 @@
+package com.cmcc.syw.basic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 演示正确地重写equals和hashCode的方法
+ * <p>
+ * Created by sunyiwei on 2016/12/21.
+ */
+public class RightEquals extends WrongEquals {
+    public RightEquals(String value) {
+        super(value);
+    }
+
+    public static void main(String[] args) {
+        RightEquals we1 = new RightEquals("hello");
+        RightEquals we2 = new RightEquals("world");
+
+
+        Map<WrongEquals, Integer> map = new HashMap<>();
+        map.put(we1, 10);
+        map.put(we2, 10);
+
+        //这里输出的值为2， 说明两个key确实都放到map中了
+        System.out.println(map.size());
+
+        //但这里输出的不为null，说明根据value值查找正常了
+        System.out.println(map.get(new RightEquals("hello")));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
---
+++ src/main/java/com/cmcc/syw/basic/WrongEquals.java
@@ -0,0 +1,46 @@
+package com.cmcc.syw.basic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试equals方法和hashCode方法
+ * <p>
+ * <p>
+ * 这里只改写了equals方法，但没有改写hashCode方法，意味着这个类在作为map的key值时会有问题
+ * <p>
+ * Created by sunyiwei on 2016/12/21.
+ */
+public class WrongEquals {
+    protected final String value;
+
+    public WrongEquals(String value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        WrongEquals we1 = new WrongEquals("hello");
+        WrongEquals we2 = new WrongEquals("world");
+
+
+        Map<WrongEquals, Integer> map = new HashMap<>();
+        map.put(we1, 10);
+        map.put(we2, 10);
+
+        //这里输出的值为2， 说明两个key确实都放到map中了
+        System.out.println(map.size());
+
+        //但这里输出的为null，说明根据value值查找出问题了
+        System.out.println(map.get(new WrongEquals("hello")));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof WrongEquals)) {
+            return false;
+        }
+
+        WrongEquals we = (WrongEquals) obj;
+        return we == this || value.equals(we.value);
+    }
+}
